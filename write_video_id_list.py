c5db9b5ceb695de7c251f9517aede20e359a5ca2
David Shulman <dshulman130@gmail.com>
2020-09-06 02:19:19 -0400

    Tried to find a smarter way to write JSON to the file, did not figure it out yet. It seems like I need to find some kind of streaming I/O library for JSON to handle this much data, as it's getting corrupted about halfway through writing.
    Signed-off-by: David Shulman <dshulman130@gmail.com>

--- write_video_id_list.py
+++ write_video_id_list.py
@@ -20,10 +20,12 @@ yt_api_file = 'youtube-api-key'
 def is_file_empty(filename):
     with open(filename) as empty_file:
         first_char = empty_file.read(1)
+        empty_file.close()
         if not first_char:
-            print('file is empty')
+            print('file' + filename + ' is empty')
             return True
         else:
+            print('file' + filename + ' is NOT empty')
             return False
 
 
@@ -31,16 +33,18 @@ def get_youtube_api(filename):
     api_key = ''
     with open(filename, 'r') as yt_file:
         api_key = yt_file.read().replace('\n', '')
+        yt_file.close()
+
     return api_key
 
 
-def get_video_ids_from_api(current_ids):
+def get_video_ids_from_api(current_video_ids):
     next_page = ''
     ids = youtube.get_videos_from_playlist_id(playlist_id, next_page)
 
     ret_ids = []
     for single_id in ids:
-        if single_id not in current_ids:
+        if single_id not in current_video_ids:
             ret_ids.append(single_id)
 
     return ret_ids
@@ -48,52 +52,91 @@ def get_video_ids_from_api(current_ids):
 
 def construct_video_id_array(video_data_file):
     with open(video_data_file, 'r') as file:
-        data = json.load(file)
+        json_data = json.load(file)
+        file.close()
 
     video_ids_separate = []
-    for video in data:
+    for video in json_data['video_data']:
         video_ids_separate.append(video['video_id'])
 
     return video_ids_separate
 
 
-def write_video_ids_to_file(video_ids, video_data_file):
-    # clear file, maybe check if it's empty first before doing this
-    file_is_empty = is_file_empty(video_data_file)
-    if file_is_empty:
-        open(video_data_file, 'w').close()
+def create_valid_json_file(key_string, file_name):
+    with open(file_name, 'r+') as file:
+        first_line = file.readline()
+        if key_string not in first_line:
+            content = file.read()
+            file.seek(0, 0)
+            file.truncate(0)  # clear file
+            file.write('{\"'+key_string+'\":[\n'+content)
+            print('formatting file properly')
+        file.close()
 
-    with open(video_data_file, 'a') as file:
-        json.dump(video_ids, file, indent=4, sort_keys=True, default=str)
-        print('collecting video ids')
+    with open(file_name, 'rb+') as file:
+        file.seek(file.tell()-1, 2)
+        last_line = file.read()
+        if '}'.encode() not in last_line:
+            file.write("}".encode())
+        file.close()
 
 
-def get_video_snippet_data(video_data_file):
+def get_video_snippet_data(video_data_file, current_snippets_file):
     with open(video_data_file, 'r') as data_file:
         video_ids_json = json.load(data_file)
+        data_file.close()
 
     counter = 0
     video_snippets_data = []
+    current_snippet_video_ids = find_non_duplicate_snippets(current_snippets_file)
     for video in video_ids_json:
         video_id = str(video['video_id'])
-        # time.sleep(1)  # Sleep for 1 second so Google doesn't get mad and close the connection
-        snippet = youtube.get_video_metadata(video_id, part=['snippet'])
-        video_snippets_data.append(snippet)
-        print('video #'+str(counter)+', processing snippet for: '+video_id)
-        counter += 1
+
+        if video_id not in current_snippet_video_ids:
+            # time.sleep(1)  # Sleep for 1 second so Google doesn't get mad and close the connection
+            snippet = youtube.get_video_metadata(video_id, part=['snippet'])
+            video_snippets_data.append(snippet)
+            print('video #'+str(counter)+', processing snippet for: '+video_id)
+            counter += 1
+        else:
+            print('video '+video_id+' is already in snippets file')
 
     return video_snippets_data
 
 
-def write_video_snippets_to_file(video_snippets_data, snippets_file_name):
-    # clear file
-    open(snippets_file_name, 'w').close()
+def find_non_duplicate_snippets(current_snippets_file, current_video_ids):
+    with open(current_snippets_file, 'r') as file:
+        current_snippets = json.load(file)
+        file.close()
 
-    with open(snippets_file_name, 'a') as file:
-        print('dumping snippets to file')
-        file.write('{')
+    snippet_video_ids = []
+    for snippet in current_snippets:
+        snippet_video_ids.append(snippet['video_id'])
+
+    return snippet_video_ids
+
+
+def write_video_snippets_to_file(video_snippets_data, snippets_file_name):
+    # create and clear file if it's empty
+    file_is_empty = is_file_empty(snippets_file_name)
+    if file_is_empty:
+        open(snippets_file_name, 'w').close()
+
+    with open(snippets_file_name, 'r+') as file:
         json.dump(video_snippets_data, file, indent=4, sort_keys=True, default=str)
-        file.write('}')
+        print('dumping snippets to file')
+        file.close()
+
+    create_valid_json_file('video_snippets', snippets_file_name)
+
+
+def write_video_ids_to_file(video_ids, video_data_file):
+    with open(video_data_file, 'r+') as file:
+        json.dump(video_ids, file, indent=4, sort_keys=True, default=str)
+        print('collecting video ids')
+        file.close()
+
+    create_valid_json_file('video_data', video_data_file)
 
 
 if __name__ == "__main__":
@@ -102,6 +145,11 @@ if __name__ == "__main__":
     yt_api = get_youtube_api(yt_api_file)
     youtube = YouTubeDataAPI(yt_api)
     video_id_file = 'video_data.json'
+    snippets_file = 'video_snippets.json'
+
+    # ensure files have a valid key at the beginning
+    create_valid_json_file('video_data', video_id_file)
+    create_valid_json_file('video_snippets', snippets_file)
 
     # read current files to populate ids and prevent duplicate downloads of data
     current_ids = construct_video_id_array(video_id_file)
@@ -112,13 +160,16 @@ if __name__ == "__main__":
     # write data to file
     write_video_ids_to_file(get_video_ids, video_id_file)
 
+    ###TODO alright so we have a different issue now: this very large JSON is not being handled well by Python
+    # we need to get a library that acts upon it as a stream instead, as there are some corruptions about halfway
+    # through writing the file
+
     ###TODO ok so the video ids are handling duplicates well now, just need to do that for snippets vs. the video_data file
     # get snippets for each video using ids
     # this contains meta data about each video useful for data labeling and organization later
-    snippets = get_video_snippet_data(video_id_file)
+    snippets = get_video_snippet_data(video_id_file, snippets_file)
 
     # write snippets to file
-    snippets_file = 'video_snippets.json'
     write_video_snippets_to_file(snippets, snippets_file)
 
     ###TODO insert section in snippets data for storing the transcription data
